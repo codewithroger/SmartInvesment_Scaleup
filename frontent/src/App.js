80fb4ef8164a407c22c00483b0f4ec3d8578915a
Yogesh Hitnalli <yogeshhitnalli96@gmail.com>
2025-03-29 16:00:06 +0530

    in the dashboard

--- frontent/src/App.js
+++ frontent/src/App.js
@@ -11,7 +11,7 @@ function App() {
         <Router>
             <Navbar />
             <Routes>
-                <Route path="/home" element={<Dashboard />} />
+                <Route path="/" element={<Dashboard />} />
                 <Route path="/investor" element={<InvestorPage />} />
                 <Route path="/startup" element={<StartupPage />} />
                 <Route path="/about" element={<About/>} />
